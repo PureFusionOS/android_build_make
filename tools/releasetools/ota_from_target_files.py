fa1d910a83369234ca6775ade5e725f39fb00520
Chris Soyars <ctso@ctso.me>
2010-12-23 00:44:33 +0100

    [build/make] [FIXED] releasetools: squash backuptool support
    this is a squash of the following commits:
    
    commit 30e43f85f8b83dd4210b86363ab912dd640050a0
    Author: Chris Soyars <ctso@ctso.me>
    Date:   Thu Dec 23 00:44:33 2010 +0100
    
        Add otapackage support for backuptool
    
        Change-Id: I512554c579d444067cd3ccbb0e6946a5eb6bc964a
    
        Modular backuptool.sh.  Executes backup and restore methods defined in arbitrary /system/addon.d/*.sh scripts.
    
        * Copy backuptool.functions alongside backuptool.sh.
        * Delete both from /system/bin as they are not useful there.
    
        Patch Series
        ============
        http://review.cyanogenmod.com/#change,13265
        CyanogenMod/android_build
          * edify generator
        http://review.cyanogenmod.com/#change,13266
        CyanogenMod/android_system_core
          * permissions on /system/addon.d
        http://review.cyanogenmod.com/#change,13267
        CyanogenMod/android_vendor_cm
          * 50-cm.sh reference backup script
          * modular backuptool.sh
          * support backuptool.functions used by /system/addon.d/*.sh scripts
    
        Change-Id: I26b4907d28f49c69627699d2accd2f0fa2d1b112
    
        update ota_from_target_files to handle mounting/unmounting for backupscript
    
        backupscript should not be mounting/unmounting itself as it makes other
        scripts have unexpected results (such as modelid_cfg, which expects /system
        to be mounted)
    
        instead have the ota script handle the mounting/unmounting
    
        Change-Id: I94511f4147c624d975cb3ecbeaa8b0e98f63437c
    
        build: Don't run backuptool on GMS builds
    
        Change-Id: I5dde27f9d16b88049171db9805221d92e67f3e5d
    
    commit 66ce73d1ffa81b9dd59639241f2e7172e5485a41
    Author: Tom Marshall <tdm@cyngn.com>
    Date:   Wed Apr 9 16:14:07 2014 -0700
    
        build: edify: use set_metadata for backuptool
    
        Also use saner permissions on backuptool.sh
    
        Change-Id: I50742b51867aa358f5924b8dc208833092a35bd9
    
    commit dac6e8fee851433ff35723f2a790e3930aaeb113
    Author: Ricardo Cerqueira <ricardo@cyngn.com>
    Date:   Wed Nov 5 18:16:43 2014 +0000
    
        ota: Include copies of the recovery scripts even if shipping in block mode
    
    commit 5a6d5b87d44f638307531f29c89d19b41c79532b
    Author: Ricardo Cerqueira <ricardo@cyngn.com>
    Date:   Wed Nov 5 19:58:59 2014 +0000
    
        ota: Fix recovery-script cleanups in block mode
    
        Change-Id: Iadb480f8062cfb7a0e6c9024e32ac0d71a7481d7
    
    commit d98d55edeec9d7d422b9c33883936d80da84acfb
    Author: Tom Marshall <tdm@cyngn.com>
    Date:   Fri Dec 12 11:51:33 2014 -0800
    
        build: ota: Support for install tools in /tmp/install
    
         * Anything in OUT/install gets packaged up into the zip and extracted
           to /tmp/install immediately after FullOTA_InstallBegin.
    
         * Use /tmp/install in edify scripts and remove code related to using
           and manipulating /system for install tools.
    
        Change-Id: I315a3238e36c8d15e26f935e272f7e27dd59c320
    
    Change-Id: I2401d334d8e16d18d3770b339ec2c787dd8e477c
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -130,6 +130,10 @@ Usage:  ota_from_target_files [flags] input_target_files output_ota_package
 
   --payload_signer_args <args>
       Specify the arguments needed for payload signer.
+
+  --backup <boolean>
+      Enable or disable the execution of backuptool.sh.
+      Disabled by default.
 """
 
 from __future__ import print_function
@@ -182,6 +186,7 @@ OPTIONS.payload_signer = None
 OPTIONS.payload_signer_args = []
 OPTIONS.extracted_input = None
 OPTIONS.key_passwords = []
+OPTIONS.backuptool = False
 
 METADATA_NAME = 'META-INF/com/android/metadata'
 UNZIP_PATTERN = ['IMAGES/*', 'META/*']
@@ -535,6 +540,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   common.CheckSize(boot_img.data, "boot.img", OPTIONS.info_dict)
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
+
+  if OPTIONS.backuptool:
+    script.ShowProgress(0.02, 10)
+    script.Mount("/system")
+    script.RunBackup("restore")
+    script.Unmount("/system")
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
@@ -1357,6 +1368,8 @@ def main(argv):
       OPTIONS.payload_signer_args = shlex.split(a)
     elif o == "--extracted_input_target_files":
       OPTIONS.extracted_input = a
+    elif o in ("--backup"):
+      OPTIONS.backuptool = bool(a.lower() == 'true')
     else:
       return False
     return True
@@ -1388,6 +1401,7 @@ def main(argv):
                                  "payload_signer=",
                                  "payload_signer_args=",
                                  "extracted_input_target_files=",
+                                 "backup=",
                              ], extra_option_handler=option_handler)
 
   if len(args) != 2:
