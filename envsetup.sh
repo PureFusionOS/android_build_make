21c6f36228bba338270ddd9d41e85a7c8ff2be44
blinoff82 <blinov.in@gmail.com>
2017-10-09 22:07:59 +0300

    envsetup: stop jack server once build completed
    Change-Id: I810e8314d7b5824f5f1a2e9da10eefd4cacf8cdc
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1862,6 +1862,7 @@ function mk_timer()
         printf "(%s seconds)" $secs
     fi
     printf " ####${color_reset}\n\n"
+    prebuilts/sdk/tools/jack-admin stop-server 2>&1 >/dev/null
     return $ret
 }
 
