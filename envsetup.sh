a1674a6dd14eefed02979a3aa6b1fb2f818e16ca
Eliminater74 <eliminater74@gmail.com>
2017-10-31 16:10:44 -0400

    [build/make] Package Completion Style up:
    Change-Id: I3d90f559c4c636acd3f6e8f0429707ec344b5d3f
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1632,7 +1632,7 @@ function mka() {
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
             ;;
         *)
-            schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
+            mk_timer schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
             ;;
     esac
 }
@@ -1712,11 +1712,11 @@ function get_make_command()
   echo command make
 }
 
-function make()
+function mk_timer()
 {
     export TARGET_DEVICE_DIR="$(find device/ -maxdepth 2 -name ${TARGET_PRODUCT/aosp_/} -type d)"
     local start_time=$(date +"%s")
-    $(get_make_command) "$@"
+    $@
     local ret=$?
     local end_time=$(date +"%s")
     local tdiff=$(($end_time-$start_time))
@@ -1735,9 +1735,9 @@ function make()
     fi
     echo
     if [ $ret -eq 0 ] ; then
-        echo -n "${color_success}#### make completed successfully "
+        printf "${color_success}#### make completed successfully "
     else
-        echo -n "${color_failed}#### make failed to build some targets "
+        printf "${color_failed}#### make failed to build some targets "
     fi
     if [ $hours -gt 0 ] ; then
         printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
@@ -1746,9 +1746,14 @@ function make()
     elif [ $secs -gt 0 ] ; then
         printf "(%s seconds)" $secs
     fi
-    echo " ####${color_reset}"
-    echo
+    printf " ####${color_reset}\n\n"
     return $ret
+}
+
+function make()
+{
+    mk_timer $(get_make_command) "$@"
+
 }
 
 function provision()
@@ -1778,6 +1783,9 @@ function provision()
         fi
     fi
     "$ANDROID_PRODUCT_OUT/provision-device" "$@"
+    echo " ####${color_reset}"
+    echo
+    return $ret
 }
 
 if [ "x$SHELL" != "x/bin/bash" ]; then
