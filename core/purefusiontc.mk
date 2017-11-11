26e87de3f96d231608eb136f1b59253ff22ad86d
Eliminater74 <eliminater74@gmail.com>
2017-11-10 21:52:43 -0500

    [build/make] PureFusionTC: Update and Re-Enable Optimizations:
    Change-Id: Ia7dcd2065b013097002afa549feee09cb6b3c0be
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- core/purefusiontc.mk
+++ core/purefusiontc.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014-2017 UBER
+# Copyright (C) 2018 PureFusionOS 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,21 +15,29 @@
 
 ####################################################
 ####################################################
-## * * * PureFusionOS [Updated: 09/28/2017] * * * ##
+## * * * PureFusionOS [Updated: 11/10/2017] * * * ##
 ####################################################
 ####################################################
 
+## Enable True/False To Use: #######################
+####################################################
+## STRICT_ALIASING := true/false
+## ENABLE_PFTC_IPA := true/false
+## ENABLE_PFTC_OPENMP := true/false
+## ENABLE_PFTC_LTO := true/false
+###################################################
+
 # Set Bluetooth Modules
 BLUETOOTH := libbluetooth_jni bluetooth.mapsapi bluetooth.default bluetooth.mapsapi libbt-brcm_stack audio.a2dp.default libbt-brcm_gki libbt-utils libbt-qcom_sbc_decoder \
-                libbt-brcm_bta libbt-brcm_stack libbt-vendor libbtprofile libbtdevice libbtcore bdt bdtest libbt-hci libosi ositests libbluetooth_jni net_test_osi net_test_device \
-                net_test_btcore net_bdtool net_hci bdAddrLoader 
+		libbt-brcm_bta libbt-brcm_stack libbt-vendor libbtprofile libbtdevice libbtcore bdt bdtest libbt-hci libosi ositests libbluetooth_jni net_test_osi net_test_device \
+		net_test_btcore net_bdtool net_hci bdAddrLoader
 
 # PureFusionOS module disable
 DISABLE_FUSION_arm :=
 DISABLE_FUSION_arm64 := libm libdng_sdk libdng% libjni_filtershow_filters busybox libfdlibm libhistory% sensorservice libwfds libsensorservice  \
-                        libv8base libhevcdec libjni_eglfence% libjni_jpegstream% libjni_gallery_filters% libLLVMAArch64CodeGen libLLVMAnalysis \
-                        librsjni libblasV8 libF77blasV8 libF77blas libRSSupport% libclcore libLLVMCodeGen libbnnmlowpV8 libLLVMARMCodeGen libLLVM% \
-                        libplatformprotos toybox libcxx libjni_latinime libjni_latinime_common_static libjni_snapcammosaic
+			libv8base libhevcdec libjni_eglfence% libjni_jpegstream% libjni_gallery_filters% libLLVMAArch64CodeGen libLLVMAnalysis \
+			librsjni libblasV8 libF77blasV8 libF77blas libRSSupport% libclcore libLLVMCodeGen libbnnmlowpV8 libLLVMARMCodeGen libLLVM% \
+			libplatformprotos
 
 # Set DISABLE_FUSION based on arch
 DISABLE_FUSION := \
@@ -168,12 +176,14 @@ my_conlyflags := $(filter-out -Wall -Werror -g -Wextra -Weverything $(O_FLAGS),$
 # IPA #
 #######
 
-ifndef LOCAL_IS_HOST_MODULE
-  ifeq (,$(filter true,$(my_clang)))
-#    my_cflags += -fipa-pta
-  else
-#    my_cflags += -analyze -analyzer-purge
-  endif
+ifeq ($(ENABLE_PFTC_IPA),true)
+     ifndef LOCAL_IS_HOST_MODULE
+          ifeq (,$(filter true,$(my_clang)))
+               my_cflags += -fipa-pta
+               else
+               my_cflags += -analyze -analyzer-purge
+          endif
+     endif
 endif
 
 ##########
@@ -194,12 +204,14 @@ LOCAL_DISABLE_OPENMP := \
 	libjni_snapcammosaic \
 	toybox \
 	mdnsd
-
-ifndef LOCAL_IS_HOST_MODULE
-  ifneq (1,$(words $(filter $(LOCAL_DISABLE_OPENMP),$(LOCAL_MODULE))))
-#    my_cflags += -lgomp -lgcc -fopenmp
-#    my_ldflags += -fopenmp
-  endif
+	
+ifeq ($(ENABLE_PFTC_OPENMP),true)
+     ifndef LOCAL_IS_HOST_MODULE
+          ifneq (1,$(words $(filter $(LOCAL_DISABLE_OPENMP),$(LOCAL_MODULE))))
+                my_cflags += -lgomp -lgcc -fopenmp
+                my_ldflags += -fopenmp
+          endif
+     endif
 endif
 
 #################
@@ -227,64 +239,15 @@ endif
 
 #### UnUsed POLLY Options ####
 
-
 # Disable modules that dont work with Polly. Split up by arch.
 DISABLE_POLLY_arm := \
-        libicuuc \
-	libjni_imageutil \
-	libjni_snapcammosaic \
-	libjni_filtershow_filters \
-	libandroid \
-	libcrypto \
-        libcrypto_static \
-	libFraunhoferAAC \
-	libjpeg_static \
-	libLLVM% \
-	libopus \
-	libpdfium% \
-	libskia_static \
-	libstagefright%
+  libjpeg_static libicuuc
 
 DISABLE_POLLY_arm64 := \
-        $(DISABLE_POLLY_arm) \
-	  libjpeg_static \
-	  libicuuc \
-	  libwebp-decode \
-	  libwebp-encode \
-	  libpdfiumfxge \
-	  libskia_static \
-	  libaudioutils \
-	  libpdfium% \
-	  libLLVMSupport \
-	  libsvoxpico \
-	  libRS_internal \
-	  libvpx \
-	  libopus \
-	  libv8 \
-	  libsonic \
-	  libaudioflinger \
-	  libstagefright% \
-	  libart \
-	  libFFTEm \
-	  libRSCpuRef \
-	  libbnnmlowp \
-	  libmedia_jni \
-	  libFraunhoferAAC \
-	  libavcdec \
-	  libavcenc \
-	  libmpeg2dec \
-	  libwebrtc% \
-	  libmusicbundle \
-	  libreverb \
-	  libscrypt_static \
-	  libmpeg2dec \
-	  libcrypto_static \
-	  libjni_latinime \
-	  libcrypto \
-	  libyuv% \
-	  libjni_gallery_filters% \
-	  libjni_gallery_filters_32 \
-	  libLLVMSelectionDAG
+  $(DISABLE_POLLY_arm) libjpeg_static libicuuc libwebp-decode libwebp-encode libpdfiumfxge libskia_static libaudioutils libpdfium% \
+  libLLVMSupport libsvoxpico libRS_internal libvpx libopus libv8 libsonic libaudioflinger libstagefright% libFFTEm libRSCpuRef \
+  libbnnmlowp libmedia_jni libFraunhoferAAC libavcdec libavcenc libmpeg2dec libwebrtc% libmusicbundle libreverb libscrypt_static \
+  libmpeg2dec libcrypto_static libcrypto libyuv% libjni_gallery_filters% libjni_gallery_filters_32 libLLVMSelectionDAG
 
 # Set DISABLE_POLLY based on arch
 DISABLE_POLLY := \
@@ -294,18 +257,18 @@ DISABLE_POLLY := \
 
 # Set POLLY based on DISABLE_POLLY
 ifeq (1,$(words $(filter $(DISABLE_POLLY),$(LOCAL_MODULE))))
-#  POLLY := -O3
+  POLLY := -O3
 endif
 
 # Set POLLY based on BLUETOOTH
 ifeq (1,$(words $(filter $(BLUETOOTH),$(LOCAL_MODULE))))
-#  POLLY := -Os
+  POLLY := -Os
 endif
 
 # Set POLLY based on DISABLE_POLLY
 ifeq ($(my_32_64_bit_suffix),32)
   ifeq (1,$(words $(filter $(DISABLE_POLLY_arm64_32),$(LOCAL_MODULE))))
-#    POLLY := -O3
+    POLLY := -O3
   endif
 endif
 
@@ -313,17 +276,17 @@ ifeq ($(my_clang),true)
   ifndef LOCAL_IS_HOST_MODULE
     # Possible conflicting flags will be filtered out to reduce argument
     # size and to prevent issues with locally set optimizations.
-#    my_cflags := $(filter-out -Wall -Werror -g -O3 -O2 -Os -O1 -O0 -Og -Oz -Wextra -Weverything,$(my_cflags))
+    my_cflags := $(filter-out -Wall -Werror -g -O3 -O2 -Os -O1 -O0 -Og -Oz -Wextra -Weverything,$(my_cflags))
     # Enable -O3 and Polly if not blacklisted, otherwise use -Os.
-#    my_cflags += $(POLLY) -Qunused-arguments -Wno-unknown-warning-option -w -fuse-ld=gold
-#    my_ldflags += -fuse-ld=gold
+    my_cflags += $(POLLY) -Qunused-arguments -Wno-unknown-warning-option -w -fuse-ld=gold
+    my_ldflags += -fuse-ld=gold
   endif
 endif
 
 ifneq (1,$(words $(filter $(DISABLE_POLLY_O3),$(LOCAL_MODULE))))
   # Remove all other "O" flags to set O3
-#  my_cflags := $(filter-out -O3 -O2 -Os -O1 -O0 -Og -Oz,$(my_cflags))
-#  my_cflags += -O3
+  my_cflags := $(filter-out -O3 -O2 -Os -O1 -O0 -Og -Oz,$(my_cflags))
+  my_cflags += -O3
 else
   my_cflags += -O2
 endif
@@ -335,7 +298,7 @@ ifeq ($(my_sdclang), true)
     ifneq (1,$(words $(filter $(LOCAL_DISABLE_POLLY),$(LOCAL_MODULE))))
       # Enable POLLY only on clang
       ifneq ($(LOCAL_CLANG),false)
- #       my_cflags += $(POLLY)
+        my_cflags += $(POLLY)
         my_cflags += -Qunused-arguments
       endif
     endif
@@ -367,7 +330,7 @@ DISABLE_THINLTO := \
   $(LOCAL_DISABLE_THINLTO)
 
 # Enable LTO (currently disabled due to issues in linking, enable at your own risk)
-ifeq ($(ENABLE_DTC_LTO),true)
+ifeq ($(ENABLE_PFTC_LTO),true)
   ifeq ($(my_clang),true)
     ifndef LOCAL_IS_HOST_MODULE
       ifneq ($(LOCAL_MODULE_CLASS),STATIC_LIBRARIES)
